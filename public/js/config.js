3cf9df2917b68ddd0884969427b35bbd4d62fd85
ARZF <aarzfamirreza@gmail.com>
2025-12-22 14:20:54 +0330

    fix(config): update marketplace contract addresses for Sepolia and Base Sepolia
    chore: remove unused index_backup.html file

--- public/js/config.js
+++ public/js/config.js
@@ -2,7 +2,7 @@ const CHAINS = {
     11155111: { // Sepolia
         name: "sepolia",
         currency: "ETH",
-        marketplace: "0xD089b7B482523405b026DF2a5caD007093252b15",
+        marketplace: "0xF93f88857d375Ff390D39A0dc1f7b114D1e8BDa7",
         nft: "0xDB9d9Bb58dB6774bbD72a9cBefb483F03Db1A5Fe",
         // Network configuration for MetaMask
         chainId: '0xaa36a7', // 11155111 in hex
@@ -18,7 +18,7 @@ const CHAINS = {
     84532: { // Base Sepolia
         name: "base-sepolia",
         currency: "ETH",
-        marketplace: "0x67d374fCE79f6F0Ad297b643792733a513735a54",
+        marketplace: "0x3198628c67dAb715258E57AFE75D1a784bB7F3D3",
         nft: "0x6B15359C8dF1Cf4F6C3cB51d0788fED2A4B6aD9a",
         // Network configuration for MetaMask
         chainId: '0x14a34', // 84532 in hex
